99379339307be047bd0b09804605e2dfb4f27bcf
abhi1739 <abhinavsrivastava2022@vitbhopal.ac.in>
2025-07-11 19:42:43 +0530

    align centre

--- Auth/src/components/Login.jsx
+++ Auth/src/components/Login.jsx
@@ -9,19 +9,19 @@ function Login() {
     console.log("Password:", password);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div class="h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} class="border-4 border-blue-500  w-5px h-5px " >
         <h2>Login Page</h2>
         <div>
           <label htmlFor="">Email: </label>
-          <input type="email" value={email} onchange={(e)=>setEmail(e.target.value)} />
+          <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
         </div>
         <div>
           <label htmlFor="">Password: </label>
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div>
-          <button type="button">Login</button>
+          <button type="button" class="bg-red-200">Login</button>
         </div>
       </form>
     </div>
